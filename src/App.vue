20243d224fc5f79a42e1865146f788c6233a065b
Simon <silvestresimon@icloud.com>
2021-06-25 16:58:43 +0200

    design

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="app">
+    <div class="calculator">
+      <div class="resultContainer">
+        <p class="result">{{ result }}</p>
+      </div>
+      <div class="keyboard">
+        <bouton number="7"/>
+        <bouton number="8"/>
+        <bouton number="9"/>
+        <bouton number="DEL" class="del"/>
+        <bouton number="4"/>
+        <bouton number="5"/>
+        <bouton number="6"/>
+        <bouton number="+"/>
+        <bouton number="1"/>
+        <bouton number="2"/>
+        <bouton number="3"/>
+        <bouton number="-"/>
+        <bouton number="."/>
+        <bouton number="0"/>
+        <bouton number="/"/>
+        <bouton number="x"/>
+        <bouton number="RESET" class="reset bigButton"/>
+        <bouton number="=" class="equal bigButton"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import bouton from './components/button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    bouton
+  },
+  data() {
+    return {
+      result: 399.981
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    background: #3b4764;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .calculator {
+    width: 500px;
+    height: 600px;
+    position: relative;
+
+    .resultContainer, .keyboard {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      width: 100%;
+      border-radius: 8px;
+    }
+    .resultContainer {
+      height: 15%;
+      background: #182032;
+      top: 0;
+      justify-content: flex-end;
+
+      .result {
+        color: #fff;
+        font-size: 40px;
+        font-weight: bold;
+        margin-right: 20px;
+      }
+    }
+    .keyboard {
+      height: 80%;
+      background: #242e44;
+      bottom: 0;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      padding: 20px 30px;
+
+      .del, .bigButton {
+        p {
+          color: #fcfcff;
+          font-size: 20px;
+        }
+      }
+      
+      .del, .reset {
+        .numberContainer {
+          background: #657199;
+        }
+        .ombre {
+          background: #424d74;
+        }
+      }
+
+      .bigButton {
+        width: 210px;
+      }
+
+      .equal {
+        .numberContainer {
+          background: #d14030;
+        }
+        .ombre {
+          background: #94261b;
+        }
+      }
+
+    }
+  }
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="button">
+    <div class="numberContainer">
+      <p>{{ number }}</p>
+    </div>
+    <span class="ombre"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    number: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.button {
+  width: 95px;
+  height: 60px;
+  position: relative;
+  cursor: pointer;
+
+  .numberContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    background: #e9e3db;
+    border-radius: 8px;
+    z-index: 1;
+    width: 100%;
+    height: 55px;
+    transition: all .1s;
+
+    &:active {
+      height: 60px;
+    }
+  }
+  p {
+    font-size: 35px;
+    font-weight: bold;
+    color: #464a59;
+  }
+  .ombre {
+    position: absolute;
+    width: 100%;
+    height: 30px;
+    background: #b2a296;
+    border-radius: 8px;
+    bottom: 0;
+  }
+}
+</style>
